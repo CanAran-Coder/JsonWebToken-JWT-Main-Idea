887f1a450ed65092da4633c39b7cae8ad750dd6a
Can Aran <Can>
2025-09-24 13:14:40 +0300

    feat:Install JWT

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import loginRoute from "./routes/loginRoute.js"
-
+import cors from "cors"
 
 const app = express();
 
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-
+app.use(cors())
 
 
 
